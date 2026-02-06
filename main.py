fe1bcc60d154de0ec7cf3ec27a5baa6d4614ebd5
BYRON LOARTE <by_tosh20@hotmail.com>
2026-02-06 15:34:39 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+num1 = 5 
+
+num2  = 10
+
+sum = num1 + num2
+
+print("La suma de los valores es ",sum)
+
+
+input("Presiona Enter para salir...")
+# pip
+# ramdom
+# os
+# datetime
+# webbrowser
+# smtplib
+# https://www.marketingschoolecuador.com/generador-api-whatsapp/
+# reportlab
+# pyttsx3
+# stdiomask
+# prettytable 
+# pyqrcode
+# pyinstaller
+# pygmae - godot - gdscript
